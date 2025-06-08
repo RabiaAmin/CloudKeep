bf16ab095e38fa100052ce69891bd45027e30055
RabiaAmin <rabia10march@gmail.com>
2025-06-08 22:31:21 +0200

    setup user registration

---
+++ routes/user.routes.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const userModel = require('../models/user.model')
+const bycrypt = require('bcrypt');
+
+// /user/register/
+router.get('/register',(req,res)=>{
+    res.render('register')
+})
+
+router.post('/register',
+    body('email').trim().isEmail().isLength({min:10}),
+    body('password').trim().isLength({min: 5}),
+    body('username').trim().isLength({min:3})
+    ,async (req,res)=>{
+
+     const errors = validationResult(req);
+
+     if(!errors.isEmpty()){
+       return  res.status(400).json({
+        errors:errors.array(),
+        message:'Invalid data'
+       })
+     }
+ 
+    const {email,username,password} = req.body;
+
+    const hashPassword = await bycrypt.hash(password,10);
+    const newUser = await userModel.create({
+        email,username,password:hashPassword
+    })
+
+    res.json(newUser)
+})
+
+
+
+module.exports = router; 
